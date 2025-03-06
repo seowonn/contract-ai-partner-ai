235faca730ea032799d9f4cdb42e87975d0f0e4a
seowonn <sonseowon5@gmail.com>
2025-03-06 15:42:10 +0900

    :recycle: dataclass로 객체형으로 데이터 관리하게 수정

---
+++ app/blueprints/document_management/models.py
@@ -0,0 +1,7 @@
+from dataclasses import dataclass
+
+@dataclass
+class PDFRequest:
+  s3_path: str
+  category: str
+  reference_id: str
--- app/blueprints/document_management/reference_document.py
+++ app/blueprints/document_management/reference_document.py
@@ -1,39 +1,41 @@
 import io
-import os.path
-import time
-import uuid
 
 from flask import Blueprint, request, jsonify
-from app.services import s3_service
-from app.services import pdf_service, chunking
+
+from app.blueprints.document_management.models import PDFRequest
+from app.services import s3_service, pdf_service, chunking
+from app.services import vector_store
 
 bp = Blueprint('reference_document', __name__, url_prefix="/flask/reference-document")
 
 @bp.route('', methods=['POST'])
 def process_pdf_from_s3():
-  data = request.json
-  s3_path = data.get("s3Path") # 없으면 None을 반환한다.
+  data = request.get_json()
 
-  if not s3_path:
-    return jsonify({"error": "Missing s3Path"}), 400
+  try:
+    pdf_request = PDFRequest(
+        s3_path=data["s3Path"],
+        category=data["category"],
+        reference_id=data["id"]
+    )
+  except (KeyError, ValueError) as e:
+    return jsonify({"error": f"Invalid request: {str(e)}"}), 400
 
   try:
     # 1️⃣ s3에서 문서(pdf) 가져오기 (메모리 내)
-    s3_stream = s3_service.s3_get_object(s3_path)
+    s3_stream = s3_service.s3_get_object(pdf_request.s3_path)
 
     if s3_stream is None:
-      raise FileNotFoundError(f"S3에서 파일을 찾을 수 없습니다: {s3_path}")
+      raise FileNotFoundError(f"S3에서 파일을 찾을 수 없습니다: {pdf_request.s3_path}")
 
     pdf_bytes_io = io.BytesIO(s3_stream.read())
     pdf_bytes_io.seek(0)
 
     # 2️⃣ PDF에서 텍스트 추출
     extracted_text = pdf_service.extract_text_from_pdf_io(pdf_bytes_io)
-    print(extracted_text)
 
     # 3️⃣ 텍스트 청킹
     chunks = chunking.chunk_by_article_and_clause(extracted_text)
-    print(chunks)
 
     return jsonify({"chunks": chunks})
 
