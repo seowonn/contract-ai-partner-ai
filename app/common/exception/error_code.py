c84899c1700e63dfd6a929ff560909c70abe65b3
seowonn <sonseowon5@gmail.com>
2025-03-10 11:15:20 +0900

    :memo: s3 client 관련 에러 코드 추가

--- app/common/exception/error_code.py
+++ app/common/exception/error_code.py
@@ -5,12 +5,12 @@ from http import HTTPStatus
 class ErrorCode(Enum):
   # standard 관련 에러
   REQUEST_NOT_MATCH = (HTTPStatus.BAD_REQUEST, "S001", "전달받은 인자를 매핑할 수 없음")
-  S3FILE_NOT_FOUND = (HTTPStatus.BAD_REQUEST, "S002", "S3에서 파일을 찾을 수 없음")
-  UPLOAD_FAIL = (HTTPStatus.INTERNAL_SERVER_ERROR, "S003", "기준문서 저장 과정 중 에러 발생")
+  UPLOAD_FAIL = (HTTPStatus.INTERNAL_SERVER_ERROR, "S002", "기준 문서 저장 과정 중 에러 발생")
 
   # common 에러
   DATA_TYPE_NOT_MATCH = (HTTPStatus.BAD_REQUEST, "C001", "데이터의 형식이 맞지 않음")
-
+  S3FILE_NOT_LOAD = (HTTPStatus.BAD_REQUEST, "C002", "S3에서 파일을 가져 오지 못함")
+  S3_CLIENT_ERROR = (HTTPStatus.INTERNAL_SERVER_ERROR, "C003", "S3 연결 도중 에러 발생")
 
   def __init__(self, status: HTTPStatus, code: str, message: str):
     self.status = status
