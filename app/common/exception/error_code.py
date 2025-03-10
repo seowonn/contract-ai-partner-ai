26efe6d35c966d9ed462b29d4780849e76f63446
seowonn <sonseowon5@gmail.com>
2025-03-10 17:36:36 +0900

    :bug: 에러 세부 핸들링 추가

--- app/common/exception/error_code.py
+++ app/common/exception/error_code.py
@@ -9,8 +9,11 @@ class ErrorCode(Enum):
 
   # common 에러
   DATA_TYPE_NOT_MATCH = (HTTPStatus.BAD_REQUEST, "C001", "데이터의 형식이 맞지 않음")
-  S3FILE_NOT_LOAD = (HTTPStatus.BAD_REQUEST, "C002", "S3에서 파일을 가져 오지 못함")
+  FILE_LOAD_FAILED = (HTTPStatus.BAD_REQUEST, "C002", "S3에서 파일을 가져 오지 못함")
   S3_CLIENT_ERROR = (HTTPStatus.INTERNAL_SERVER_ERROR, "C003", "S3 연결 도중 에러 발생")
+  CONVERT_TO_IO_FAILED = (HTTPStatus.INTERNAL_SERVER_ERROR, "C004", "스트림을 io 바이트로 변환하는데 에러 발생")
+  INNER_DATA_ERROR = (HTTPStatus.INTERNAL_SERVER_ERROR, "C005", "페이지가 없거나 내부 구조가 깨져 있음")
+  FILE_FORMAT_INVALID = (HTTPStatus.INTERNAL_SERVER_ERROR, "C006", "파일이 깨졌 거나 지원 불가입니다.")
 
   def __init__(self, status: HTTPStatus, code: str, message: str):
     self.status = status
--- app/services/common/pdf_service.py
+++ app/services/common/pdf_service.py
@@ -1,30 +1,51 @@
 import pdfplumber
 import io
 
+from pdfminer.pdfparser import PDFSyntaxError
+
 from app.common.exception.custom_exception import BaseCustomException
 from app.common.exception.error_code import ErrorCode
 
 
+def convert_to_bytes_io(s3_stream):
+  try:
+    pdf_bytes_io = io.BytesIO(s3_stream.read())
+    pdf_bytes_io.seek(0)
+    return pdf_bytes_io
+  except Exception:
+    raise BaseCustomException(ErrorCode.CONVERT_TO_IO_FAILED)
+
+
 def extract_text_from_pdf_io(pdf_bytes_io):
-    if not isinstance(pdf_bytes_io, io.BytesIO):
-        raise BaseCustomException(ErrorCode.DATA_TYPE_NOT_MATCH)
+  if not isinstance(pdf_bytes_io, io.BytesIO):
+    raise BaseCustomException(ErrorCode.DATA_TYPE_NOT_MATCH)
 
-    extracted_text = []
+  extracted_text = []
 
+  try:
     with pdfplumber.open(pdf_bytes_io) as pdf:
-        for page in pdf.pages:
-            text = page.extract_text()
-            if text:
-                extracted_text.append(text.strip())
+      for page in pdf.pages:
+        try:
+          text = page.extract_text()
+          if text:
+            extracted_text.append(text.strip())
+        except (AttributeError, TypeError):
+          raise BaseCustomException(ErrorCode.INNER_DATA_ERROR)
+  except (PDFSyntaxError, ValueError):
+    raise BaseCustomException(ErrorCode.FILE_FORMAT_INVALID)
 
-    return "\n".join(extracted_text) # 리스트 사용 문자열 결합 최적화
+  return "\n".join(extracted_text)  # 리스트 사용 문자열 결합 최적화
 
 
 def extract_text_from_pdf_path(pdf_path):
+  try:
     with pdfplumber.open(pdf_path) as pdf:
-        for page in pdf.pages:
-            text = page.extract_text()
-    return text
+      for page in pdf.pages:
+        text = page.extract_text()
+  except (PDFSyntaxError, ValueError):
+    raise BaseCustomException(ErrorCode.FILE_FORMAT_INVALID)
+
+  return text
 
 
 
