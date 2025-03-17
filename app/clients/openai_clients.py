0baae7ae8ec60baa126e407ff39022ba513aec3d
seowonn <sonseowon5@gmail.com>
2025-03-17 12:01:39 +0900

    :art: api version env에서 관리하게 변경

--- app/clients/openai_clients.py
+++ app/clients/openai_clients.py
@@ -7,7 +7,7 @@ load_dotenv() # 루트로 고정
 
 embedding_client = openai.AzureOpenAI(
     api_key=os.getenv("AZURE_EMBEDDING_API_KEY"),
-    api_version="2023-05-15",
+    api_version=os.getenv("AZURE_EMBEDDING_OPENAI_API_VERSION"),
     azure_endpoint=os.getenv("AZURE_EMBEDDING_OPENAI_ENDPOINT")
 )
 embedding_deployment_name = os.getenv("AZURE_EMBEDDING_OPENAI_DEPLOYMENT_NAME")
@@ -15,7 +15,7 @@ embedding_deployment_name = os.getenv("AZURE_EMBEDDING_OPENAI_DEPLOYMENT_NAME")
 
 prompt_client = openai.AzureOpenAI(
     api_key=os.getenv("AZURE_PROMPT_API_KEY"),
-    api_version="2024-10-21",
+    api_version=os.getenv("AZURE_PROMPT_OPENAI_API_VERSION"),
     azure_endpoint=os.getenv("AZURE_PROMPT_OPENAI_ENDPOINT")
 )
 prompt_deployment_name = os.getenv("AZURE_PROMPT_OPENAI_DEPLOYMENT_NAME")
