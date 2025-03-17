ad4538822c77a11559eb146352e8c32b5dfb3d9a
seowonn <sonseowon5@gmail.com>
2025-03-17 14:45:08 +0900

    :memo: 배포 환경 설정 pywin 주석! 로킹 lvl warning

--- app/__init__.py
+++ app/__init__.py
@@ -21,7 +21,7 @@ def create_app():
   # 로깅 설정
   werkzeug_logger = logging.getLogger('werkzeug')
   werkzeug_logger.disabled = True
-  logging.basicConfig(level=logging.DEBUG)
+  logging.basicConfig(level=logging.WARNING)
 
   @app.route('/healthz', methods=['GET'])
   def health_check():
