bee5b4a7aa05354cb767caf5cbdc1a62e23ce9a6
seowonn <sonseowon5@gmail.com>
2025-03-17 17:37:28 +0900

    🩹 standard -> standards

--- app/blueprints/standard/standard_blueprint.py
+++ app/blueprints/standard/standard_blueprint.py
@@ -1,16 +1,13 @@
 from http import HTTPStatus
 
 from flask import Blueprint, request
+from pydantic import ValidationError
 
-from app.blueprints.standard.standard_exception import StandardException
-from app.common.exception.custom_exception import BaseCustomException
 from app.common.file_type import FileType
 from app.schemas.document_request import DocumentRequest
 from app.schemas.success_code import SuccessCode
 from app.schemas.success_response import SuccessResponse
 from app.services.standard import processor
-from pydantic import ValidationError
-
 from app.services.standard.vector_delete import delete_by_standard_id
 
 standards = Blueprint('standards', __name__, url_prefix="/flask/standards")
@@ -33,7 +30,7 @@ def process_pdf_from_s3():
   return SuccessResponse(SuccessCode.UPLOAD_SUCCESS, "success").of(), status_code
 
 
-@standard.route('/<standardId>', methods=["DELETE"])
+@standards.route('/<standardId>', methods=["DELETE"])
 def delete_standard(standardId: str):
   status_code = delete_by_standard_id(int(standardId))
   return SuccessResponse(SuccessCode.DELETE_SUCCESS, "success").of(), status_code
