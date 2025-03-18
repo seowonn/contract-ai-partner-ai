71351ccd5ce2084403094702223d8933868c19aa
cocoball97 <wlrn0514@gmail.com>
2025-03-19 08:55:57 +0900

    📝 에러코드 수정

--- app/common/exception/error_code.py
+++ app/common/exception/error_code.py
@@ -19,7 +19,7 @@ class ErrorCode(Enum):
   FILE_FORMAT_INVALID = (HTTPStatus.INTERNAL_SERVER_ERROR, "C006", "파일이 깨졌 거나 지원 불가입니다.")
 
   # agreement 관련 에러
-  REVIEW_FAIL = (HTTPStatus.BAD_REQUEST, "A001", "AI 검토 보고서 생성 작업 중 에러 발생")
+  REVIEW_FAIL = (HTTPStatus.INTERNAL_SERVER_ERROR, "A001", "AI 검토 보고서 생성 작업 중 에러 발생")
 
   def __init__(self, status: HTTPStatus, code: str, message: str):
     self.status = status
