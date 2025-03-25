707a3a34a45a1383adaf96cf195fa6807b7cbbcb
cocoball97 <wlrn0514@gmail.com>
2025-03-25 15:06:00 +0900

    ♻️ print 로그 logger로 변경

--- app/services/common/processor.py
+++ app/services/common/processor.py
@@ -14,29 +14,26 @@ import time
 
 
 def preprocess_data(document_request: DocumentRequest) -> list:
-  # 1️⃣ s3에서 문서(pdf) 가져오기 (메모리 내)
-  start_time = time.time()  # 시작 시간 기록
-  response = s3_get_object(document_request.url)
-  s3_time = time.time() - start_time  # 경과 시간 계산
-  print(f"s3_get_object took {s3_time:.4f} seconds")
-  logging.info(f"s3_get_object took {s3_time:.4f} seconds")
+    # 1️⃣ s3에서 문서(pdf) 가져오기 (메모리 내)
+    start_time = time.time()  # 시작 시간 기록
+    s3_stream = s3_get_object(document_request.url)
+    s3_time = time.time() - start_time  # 경과 시간 계산
+    logging.info(f"s3_get_object took {s3_time:.4f} seconds")
 
+    # 2️⃣ PDF 스트림으로 변환
+    start_time = time.time()  # 시작 시간 기록
+    pdf_bytes_io = convert_to_bytes_io(s3_stream)
+    convert_time = time.time() - start_time  # 경과 시간 계산
+    logging.info(f"convert_to_bytes_io took {convert_time:.4f} seconds")
 
-  # 2️⃣ PDF 스트림으로 변환
-  start_time = time.time()  # 시작 시간 기록
-  pdf_bytes_io = convert_to_bytes_io(response)
-  convert_time = time.time() - start_time  # 경과 시간 계산
-  print(f"convert_to_bytes_io took {convert_time:.4f} seconds")
-  logging.info(f"convert_to_bytes_io took {convert_time:.4f} seconds")
+    # 3️⃣ PDF에서 텍스트 추출
+    start_time = time.time()  # 시작 시간 기록
+    extracted_text = extract_text_from_pdf_io(pdf_bytes_io)
+    extract_time = time.time() - start_time  # 경과 시간 계산
+    logging.info(f"extract_text_from_pdf_io took {extract_time:.4f} seconds")
 
-  # 3️⃣ PDF에서 텍스트 추출
-  start_time = time.time()  # 시작 시간 기록
-  extracted_text = extract_text_from_pdf_io(pdf_bytes_io)
-  extract_time = time.time() - start_time  # 경과 시간 계산
-  print(f"extract_text_from_pdf_io took {extract_time:.4f} seconds")
-  logging.info(f"extract_text_from_pdf_io took {extract_time:.4f} seconds")
+    return extracted_text
 
-  return extracted_text
 
 
 def chunk_texts(extracted_text: list) -> List[ArticleChunk]:
@@ -44,7 +41,6 @@ def chunk_texts(extracted_text: list) -> List[ArticleChunk]:
   # 4️⃣ 텍스트 청킹
   chunks = chunk_by_article_and_clause_with_page(extracted_text)
   chunk_time = time.time() - start_time  # 경과 시간 계산
-  print(f"chunk_by_article_and_clause_with_page took {chunk_time:.4f} seconds")
   logging.info(
     f"chunk_by_article_and_clause_with_page took {chunk_time:.4f} seconds")
 
