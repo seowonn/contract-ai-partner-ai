910a2d255dc431f1d7c08cfa7b7d64e8214f7f5d
seowonn <sonseowon5@gmail.com>
2025-03-25 21:52:02 +0900

    :recycle: 텍스트 정보 Document 클래스로 담기
    - 텍스트 전체 원문은 documents에서 추출하게 수정
    - ingestion_pipeline 반환형 수정

--- app/services/common/ingestion_pipeline.py
+++ app/services/common/ingestion_pipeline.py
@@ -3,17 +3,18 @@ from typing import List
 from app.common.exception.custom_exception import BaseCustomException
 from app.common.exception.error_code import ErrorCode
 from app.schemas.chunk_schema import ArticleChunk
+from app.schemas.document import Document
 from app.schemas.document_request import DocumentRequest
 from app.services.common.chunking_service import chunk_by_article_and_clause_with_page
 from app.services.common.pdf_service import convert_to_bytes_io, \
-  extract_text_and_documents_from_pdf_io
+  extract_documents_from_pdf_io
 from app.services.common.s3_service import s3_get_object
 
 import logging
 import time
 
 
-def preprocess_data(document_request: DocumentRequest) -> list:
+def preprocess_data(document_request: DocumentRequest) -> List[Document]:
     # 1️⃣ s3에서 문서(pdf) 가져오기 (메모리 내)
     start_time = time.time()  # 시작 시간 기록
     s3_stream = s3_get_object(document_request.url)
@@ -28,11 +29,11 @@ def preprocess_data(document_request: DocumentRequest) -> list:
 
     # 3️⃣ PDF에서 텍스트 추출
     start_time = time.time()  # 시작 시간 기록
-    extracted_text = extract_text_and_documents_from_pdf_io(pdf_bytes_io)
+    documents = extract_documents_from_pdf_io(pdf_bytes_io)
     extract_time = time.time() - start_time  # 경과 시간 계산
-    logging.info(f"extract_text_from_pdf_io took {extract_time:.4f} seconds")
+    logging.info(f"extract_text_and_documents_from_pdf_io took {extract_time:.4f} seconds")
 
-    return extracted_text
+    return documents
 
 
 
--- app/services/common/pdf_service.py
+++ app/services/common/pdf_service.py
@@ -1,9 +1,11 @@
 import io
+from typing import List
+
 import fitz
 
-from pdfminer.pdfparser import PDFSyntaxError
 from app.common.exception.custom_exception import BaseCustomException
 from app.common.exception.error_code import ErrorCode
+from app.schemas.document import Document, DocumentMetadata
 
 
 def convert_to_bytes_io(s3_stream: bytes) -> io.BytesIO:
@@ -15,28 +17,25 @@ def convert_to_bytes_io(s3_stream: bytes) -> io.BytesIO:
     raise BaseCustomException(ErrorCode.CONVERT_TO_IO_FAILED)
 
 
-def extract_text_and_documents_from_pdf_io(pdf_bytes_io: io.BytesIO):
-  if not isinstance(pdf_bytes_io, io.BytesIO):
-    raise BaseCustomException(ErrorCode.DATA_TYPE_NOT_MATCH)
-
-  extracted_text = []
+def extract_documents_from_pdf_io(pdf_bytes_io: io.BytesIO) -> List[Document]:
+  documents: List[Document] = []
 
   try:
-    # PyMuPDF (fitz) 사용
-    # stream 인자가 없으면 BytesIO 인식 못하므로 필수 (파일이 아닌 메모리로부터 찾기)
     doc = fitz.open(stream=pdf_bytes_io, filetype="pdf")
-    page_count = doc.page_count
-    for page_num in range(page_count):
-      try:
-        page = doc.load_page(page_num)
-        text = page.get_text()
-        if text:
-          extracted_text.append((page_num + 1, text.strip()))  # 페이지 번호와 텍스트 튜플로 저장
-      except (AttributeError, TypeError):
-        raise BaseCustomException(ErrorCode.INNER_DATA_ERROR)
 
-  except (PDFSyntaxError, ValueError):
+    for page_num in range(doc.page_count):
+      page = doc.load_page(page_num)
+      text = page.get_text().strip()
+
+      if text:
+        meta = DocumentMetadata(page=page_num + 1)
+        documents.append(Document(
+            page_content=text,
+            metadata=meta
+        ))
+
+  except Exception:
     raise BaseCustomException(ErrorCode.FILE_FORMAT_INVALID)
 
-  return extracted_text
+  return documents
 
