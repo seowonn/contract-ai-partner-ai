636db1db57e08979eed313b5bd6def1b4c9a196d
seowonn <sonseowon5@gmail.com>
2025-03-26 16:05:14 +0900

    :recycle: 공통 함수 합치기

--- app/services/common/pdf_service.py
+++ app/services/common/pdf_service.py
@@ -3,8 +3,6 @@ from typing import List
 
 import fitz
 
-from pdfminer.pdfparser import PDFSyntaxError
-
 from app.common.exception.custom_exception import BaseCustomException
 from app.common.exception.error_code import ErrorCode
 from app.schemas.chunk_schema import Document, DocumentMetadata
@@ -19,7 +17,7 @@ def convert_to_bytes_io(s3_stream: bytes) -> io.BytesIO:
     raise BaseCustomException(ErrorCode.CONVERT_TO_IO_FAILED)
 
 
-def extract_documents_from_pdf_io2(pdf_bytes_io: io.BytesIO) -> List[Document]:
+def extract_documents_from_pdf_io(pdf_bytes_io: io.BytesIO) -> List[Document]:
   documents: List[Document] = []
 
   try:
@@ -40,29 +38,4 @@ def extract_documents_from_pdf_io2(pdf_bytes_io: io.BytesIO) -> List[Document]:
     raise BaseCustomException(ErrorCode.FILE_FORMAT_INVALID)
 
   return documents
-
-def extract_documents_from_pdf_io(pdf_bytes_io):
-  if not isinstance(pdf_bytes_io, io.BytesIO):
-    raise BaseCustomException(ErrorCode.DATA_TYPE_NOT_MATCH)
-
-  extracted_text = []
-
-  try:
-    # PyMuPDF (fitz) 사용
-    # stream 인자가 없으면 BytesIO 인식 못하므로 필수 (파일이 아닌 메모리로부터 찾기)
-    doc = fitz.open(stream=pdf_bytes_io, filetype="pdf")
-    page_count = doc.page_count
-    for page_num in range(page_count):
-      try:
-        page = doc.load_page(page_num)
-        text = page.get_text()
-        if text:
-          extracted_text.append((page_num + 1, text.strip()))  # 페이지 번호와 텍스트 튜플로 저장
-      except (AttributeError, TypeError):
-        raise BaseCustomException(ErrorCode.INNER_DATA_ERROR)
-
-  except (PDFSyntaxError, ValueError):
-    raise BaseCustomException(ErrorCode.FILE_FORMAT_INVALID)
-
-  return extracted_text
 
