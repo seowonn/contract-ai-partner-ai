266ae0e2f565be5a2b52a9d8ea2b2c9ac40c0b4e
seowonn <sonseowon5@gmail.com>
2025-03-28 16:31:21 +0900

    🎨 indent 수정, 상수 클래스 -> 모듈 수준 상수로 변경

--- app/common/constants.py
+++ app/common/constants.py
@@ -3,6 +3,7 @@ SUCCESS = "success"
 TEST_COLLECTION = "test"
 ARTICLE_HEADER_PATTERN = r'^\s*제\s*\d+\s*조'
 ARTICLE_CHUNK_PATTERN = r'(제\d+조\s*(?:【[^】]+】|\([^)]+\)))(.*?)(?=(?:제\d+조\s*(?:【[^】]+】|\([^)]+\))|$))'
+ARTICLE_HEADER_PARSE_PATTERN = r'제(\d+)조\s*(?:【([^】]+)】|\(([^)]+)\))'
 CLAUSE_TEXT_SEPARATOR = "!!!"
 ARTICLE_CLAUSE_SEPARATOR = "+"
 
--- config/app_config.py
+++ config/app_config.py
@@ -1,13 +1,14 @@
 import os
 from dotenv import load_dotenv
 
-from app.common.constants import Constants
+from app.common.constants import QDRANT_COLLECTION, TEST_COLLECTION
 
 load_dotenv()
 
-class AppConfig:
-    APP_ENV = os.getenv("APP_ENV", "dev")
-    QDRANT_HOST = "qdrant" if APP_ENV == "prod" else "localhost"
-    QDRANT_PORT = 6333
 
-    COLLECTION_NAME = Constants.QDRANT_COLLECTION.value if APP_ENV == "prod" else Constants.TEST_COLLECTION.value
+class AppConfig:
+  APP_ENV = os.getenv("APP_ENV", "dev")
+  QDRANT_HOST = "qdrant" if APP_ENV == "prod" else "localhost"
+  QDRANT_PORT = 6333
+
+  COLLECTION_NAME = QDRANT_COLLECTION if APP_ENV == "prod" else TEST_COLLECTION
