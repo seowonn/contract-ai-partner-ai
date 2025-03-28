c30463fbacfd0482de39871e412b862184673f57
seowonn <sonseowon5@gmail.com>
2025-03-28 16:45:53 +0900

    :recycle: chunk_by_article_and_clause_with_page 코드 함수 분리

--- app/common/constants.py
+++ app/common/constants.py
@@ -1,9 +1,14 @@
 QDRANT_COLLECTION = "standard"
-SUCCESS = "success"
 TEST_COLLECTION = "test"
+
+SUCCESS = "success"
+
 ARTICLE_HEADER_PATTERN = r'^\s*제\s*\d+\s*조'
 ARTICLE_CHUNK_PATTERN = r'(제\d+조\s*(?:【[^】]+】|\([^)]+\)))(.*?)(?=(?:제\d+조\s*(?:【[^】]+】|\([^)]+\))|$))'
 ARTICLE_HEADER_PARSE_PATTERN = r'제(\d+)조\s*(?:【([^】]+)】|\(([^)]+)\))'
+
+CLAUSE_HEADER_PATTERN = r'(①|1\.|\(1\))'
+
 CLAUSE_TEXT_SEPARATOR = "!!!"
 ARTICLE_CLAUSE_SEPARATOR = "+"
 
