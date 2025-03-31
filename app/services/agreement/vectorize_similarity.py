a3cfa3c932b63c1d79d92cc4522c0fd66055dd81
seowonn <sonseowon5@gmail.com>
2025-03-31 10:12:34 +0900

    :bug: None 처리 추가

--- app/services/agreement/vectorize_similarity.py
+++ app/services/agreement/vectorize_similarity.py
@@ -1,6 +1,5 @@
 import asyncio
 import logging
-from asyncio import Semaphore
 from typing import List, Optional
 
 from qdrant_client import models
@@ -21,12 +20,10 @@ async def vectorize_and_calculate_similarity(
     pdf_document: fitz.Document) -> List[RagResult]:
   await ensure_qdrant_collection(collection_name)
 
-  semaphore = asyncio.Semaphore(3)  # 딱 한 번만 생성
   tasks = []
   for chunk in sorted_chunks:
     tasks.append(process_clause(chunk, chunk.incorrect_text, collection_name,
-                                document_request.categoryName, semaphore,
-                                pdf_document))
+                                document_request.categoryName, pdf_document))
 
   # 모든 임베딩 및 유사도 검색 태스크를 병렬로 실행
   results = await asyncio.gather(*tasks)
@@ -34,48 +31,49 @@ async def vectorize_and_calculate_similarity(
 
 
 async def process_clause(rag_result: RagResult, clause_content: str,
-    collection_name: str, category_name: str, semaphore: Semaphore,
-    pdf_document: fitz.Document) -> Optional[RagResult]:
+    collection_name: str, category_name: str, pdf_document: fitz.Document) -> Optional[RagResult]:
   embedding = await embedding_service.embed_text(clause_content)
 
   client = get_qdrant_client()
-  for retry in range(2):
-    try:
-      async with semaphore:
-        search_results = await client.query_points(
-            collection_name=collection_name,
-            query=embedding,
-            query_filter=models.Filter(
-                must=[models.FieldCondition(
-                    key="category",
-                    match=models.MatchValue(value=category_name)
-                )]
-            ),
-            search_params=models.SearchParams(hnsw_ef=128, exact=False),
-            limit=5
-        )
-      break
-    except Exception as e:
-      if retry == 1:
-        raise BaseCustomException(ErrorCode.QDRANT_SEARCH_FAILED)
-      logging.warning(f"query_points: Qdrant Search 재요청 발생 {e}")
-      await asyncio.sleep(1)
+  try:
+    search_results = await client.query_points(
+        collection_name=collection_name,
+        query=embedding,
+        query_filter=models.Filter(
+            must=[models.FieldCondition(
+                key="category",
+                match=models.MatchValue(value=category_name)
+            )]
+        ),
+        search_params=models.SearchParams(hnsw_ef=128, exact=False),
+        limit=5
+    )
+  except Exception as e:
+    raise BaseCustomException(ErrorCode.QDRANT_SEARCH_FAILED)
 
 
   # 3️⃣ 유사한 문장들 처리
   clause_results = []
-  for match in search_results.points:
-    try:
-      payload_data = match.payload or {}
-      clause_results.append({
-        "id": match.id,  # ✅ 벡터 ID
-        "proof_text": payload_data.get("proof_text", ""),  # ✅ 원본 문장
-        "incorrect_text": payload_data.get("incorrect_text", ""),  # ✅ 잘못된 문장
-        "corrected_text": payload_data.get("corrected_text", "")  # ✅ 교정된 문장
-      })
-    except Exception as e:
-      logging.error(f"[process_clause]: {e}")
-      continue
+  if search_results and search_results.points:
+    for match in search_results.points:
+      try:
+        payload_data = match.payload or {}
+        clause_results.append({
+          "id": match.id,
+          "proof_text": payload_data.get("proof_text", ""),
+          "incorrect_text": payload_data.get("incorrect_text", ""),
+          "corrected_text": payload_data.get("corrected_text", "")
+        })
+      except Exception as e:
+        logging.error(f"[process_clause]: {e}")
+        continue
+  else:
+    logging.warning("[process_clause]: search_results.points 비어 있음")
+
+  if not clause_results:
+    logging.warning(
+      "[process_clause]: clause_results 비어 있음")
+    return None
 
   # 4️⃣ 계약서 문장을 수정 (해당 조항의 TOP 5개 유사 문장을 기반으로)
   corrected_result = await prompt_service.correct_contract(
