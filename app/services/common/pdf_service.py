4ff705d05d717e5036274b5d52062f6832d4592b
seowonn <sonseowon5@gmail.com>
2025-04-10 23:46:29 +0900

    :recycle: s3 ~ pdf 다운로드 코드 리팩터링

--- app/services/common/pdf_service.py
+++ app/services/common/pdf_service.py
@@ -10,36 +10,34 @@ from app.schemas.chunk_schema import Document, DocumentMetadata
 
 def convert_to_bytes_io(s3_stream: bytes) -> io.BytesIO:
   try:
-    pdf_bytes_io = io.BytesIO(s3_stream)
-    pdf_bytes_io.seek(0)
-    return pdf_bytes_io
+    return io.BytesIO(s3_stream)
   except Exception:
     raise CommonException(ErrorCode.CONVERT_TO_IO_FAILED)
 
 
-def extract_documents_from_pdf_io(pdf_bytes_io: io.BytesIO) -> List[Document]:
+def extract_fitz_document_from_pdf_io(
+    pdf_bytes_io: io.BytesIO) -> fitz.Document:
+  try:
+    return fitz.open(stream=pdf_bytes_io, filetype="pdf")
+  except Exception:
+    raise CommonException(ErrorCode.FILE_FORMAT_INVALID)
+
+
+def parse_pdf_to_documents(doc: fitz.Document) -> List[Document]:
   documents: List[Document] = []
 
   try:
-    doc = fitz.open(stream=pdf_bytes_io, filetype="pdf")
-
-    for page_num in range(doc.page_count):
-      page = doc.load_page(page_num)
+    for page in doc:
       text = page.get_text("text").strip()
 
-
       if text:
-        meta = DocumentMetadata(page=page_num + 1)
+        meta = DocumentMetadata(page=page.number + 1)
         documents.append(Document(
             page_content=text,
             metadata=meta
         ))
 
   except Exception:
-    raise CommonException(ErrorCode.FILE_FORMAT_INVALID)
+    raise CommonException(ErrorCode.PDF_LOAD_FAILED)
 
   return documents
-
-
-def byte_data(pdf_bytes_io: io.BytesIO) -> fitz.Document:
-  return fitz.open(stream=pdf_bytes_io, filetype="pdf")
