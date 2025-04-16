6f6b662a5063b5ebfb34c84b5f458556dac97732
seowonn <sonseowon5@gmail.com>
2025-04-16 16:15:19 +0900

    :memo: 계약서 청킹 정규식 띄어쓰기 포함 수정

--- app/common/constants.py
+++ app/common/constants.py
@@ -1,8 +1,8 @@
 SUCCESS = "success"
 
 ARTICLE_HEADER_PATTERN = r'^\s*제\s*\d+\s*조'
-ARTICLE_CHUNK_PATTERN = r'(제\d+조\s*(?:【[^】]+】|\([^)]+\)))(.*?)(?=(?:제\d+조\s*(?:【[^】]+】|\([^)]+\))|$))'
-ARTICLE_HEADER_PARSE_PATTERN = r'제(\d+)조\s*(?:【([^】]+)】|\(([^)]+)\))'
+ARTICLE_CHUNK_PATTERN = r'(제\s*\d+\s*조\s*(?:【[^】]+】|\([^)]+\)|\[[^\]]+\]))(.*?)(?=(?:제\s*\d+\s*조\s*(?:【[^】]+】|\([^)]+\)|\[[^\]]+\])|$))'
+ARTICLE_HEADER_PARSE_PATTERN = r'제\s*(\d+)\s*조\s*(?:【([^】]+)】|\(([^)]+)\)|\[([^\]]+)\])'
 
 CLAUSE_HEADER_PATTERN = r'(①|1\.|\(1\))'
 
