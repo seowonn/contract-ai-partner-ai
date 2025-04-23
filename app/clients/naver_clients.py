6172cc733e308bc4ff1299898dfaa6e094cf290e
seowonn <sonseowon5@gmail.com>
2025-04-23 22:37:38 +0900

    :green_heart: clova 환경 변수명 원상 복구

--- app/clients/naver_clients.py
+++ app/clients/naver_clients.py
@@ -10,8 +10,8 @@ load_dotenv()
 
 
 def get_naver_ocr_client() -> Tuple[str, dict]:
-  api_url = os.getenv("PLATFORM_API_URL")
-  api_key = os.getenv("PLATFORM_API_KEY")
+  api_url = os.getenv("NAVER_CLOVA_API_URL")
+  api_key = os.getenv("NAVER_CLOVA_API_KEY")
 
   if not api_url or not api_key:
     raise AgreementException(ErrorCode.NAVER_OCR_SETTING_LOAD_FAIL)
